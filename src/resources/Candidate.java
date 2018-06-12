8d12e0f5d4c48492b4e04ce4a42f5d7a73b97a98
Ethan Ray Mosqueda <ethanray.mosqueda@gmail.com>
2018-06-12 15:21:45 +0800

    added checkIfVoted() and getFromCandidatesWhereNameIs()

--- src/resources/Candidate.java
+++ src/resources/Candidate.java
@@ -20,11 +20,12 @@ public class Candidate {
     private String updatedBy;
     private Date updatedOn;
     
-    Candidate(String name, String pos){
+    Candidate(String name, String pos, String user){
         candidateName = name;
         position = pos;
         votes=0;
         addedOn = new Date();
+        addedBy = user;
     }
     
     public String getCandidateName(){
@@ -39,12 +40,16 @@ public class Candidate {
         return this.votes;
     }
     
-    public void setCandidateName(String name){
+    public void setCandidateName(String name, String user){
         this.candidateName = name;
+        this.updatedBy = user;
+        this.updatedOn = new Date();
     }
     
-    public void setPosition(String pos){
+    public void setPosition(String pos, String user){
         this.position = pos;
+        this.updatedBy = user;
+        this.updatedOn = new Date();
     }
     
     public void addVote(){
